6e6ce880efbde3f8c3446bd63e18242ba8999fdd
gonali <gonali@aliyun.com>
2016-09-13 10:32:00 +0800

    add UserActionController

---
+++ src/main/java/com/gonali/qiaokebao/controller/UserActionController.java
@@ -0,0 +1,16 @@
+package com.gonali.qiaokebao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by TianyuanPan on 9/13/16.
+ */
+
+@RestController
+@RequestMapping(value = "/api/userAction", method = RequestMethod.POST)
+public class UserActionController {
+
+
+}
